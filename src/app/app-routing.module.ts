53613e22606e9c560accf7815d9542d0915325d7
Kingsley Etoka <etokakingsley@gmail.com>
2022-11-13 23:18:46 +0100

    configured routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'login', loadChildren: () => import('./features/authentication/login/login.module').then(m => m.LoginModule) }];
+const routes: Routes = [
+  { 
+    path: 'login', 
+    loadChildren: () => import('./features/authentication/login/login.module').then(m => m.LoginModule) 
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
