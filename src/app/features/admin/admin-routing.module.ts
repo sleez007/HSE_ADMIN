fcbbb219f7495fc28031322ef600b524f9105366
Kingsley Etoka <etokakingsley@gmail.com>
2022-11-21 16:18:11 +0100

    cleaned up the router titles

--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -7,11 +7,11 @@ const routes: Routes = [
     children: [
       { path: 'project', title: 'HSE | ADD NEW PROJECT', loadChildren: () => import('./project/create/create.module').then(m => m.CreateModule) },
       { path: 'projects', title: 'HSE | MANAGE PROJECTS', loadChildren: () => import('./project/manage/manage.module').then(m => m.ManageModule) },
-      { path: 'create-staff', title: 'HSE | ADMIN | ADD STAFFS', loadChildren: () => import('./staff/add-staff/add-staff.module').then(m => m.AddStaffModule) },
-      { path: 'all-staffs', title: 'HSE | ADMIN | STAFFS', loadChildren: () => import('./staff/all-staffs/all-staffs.module').then(m => m.AllStaffsModule) },
-      { path: 'staff-detail', title: 'HSE | ADMIN | STAFF DETAIL', loadChildren: () => import('./staff/staff-detail/staff-detail.module').then(m => m.StaffDetailModule) },
-      { path: 'hse-incident', loadChildren: () => import('./incidents/hse-incident/hse-incident.module').then(m => m.HseIncidentModule) },
-      { path: 'cpa-incident', loadChildren: () => import('./incidents/cpa-incident/cpa-incident.module').then(m => m.CpaIncidentModule) },
+      { path: 'create-staff', title: 'HSE | ADD STAFFS', loadChildren: () => import('./staff/add-staff/add-staff.module').then(m => m.AddStaffModule) },
+      { path: 'all-staffs', title: 'HSE | STAFFS', loadChildren: () => import('./staff/all-staffs/all-staffs.module').then(m => m.AllStaffsModule) },
+      { path: 'staff-detail', title: 'HSE | STAFF DETAIL', loadChildren: () => import('./staff/staff-detail/staff-detail.module').then(m => m.StaffDetailModule) },
+      { path: 'hse-incident', title: 'HSE | HSE INCIDENTS', loadChildren: () => import('./incidents/hse-incident/hse-incident.module').then(m => m.HseIncidentModule) },
+      { path: 'cpa-incident', title: 'HSE | CPA INCIDENTS', loadChildren: () => import('./incidents/cpa-incident/cpa-incident.module').then(m => m.CpaIncidentModule) },
       { path: '', title: 'HSE | ADMIN DASHBOARD', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
     ]
   },
