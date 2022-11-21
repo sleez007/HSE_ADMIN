47cf6fe9a3dff0a00eb48b97d63cc0c9d375a7e2
Kingsley Etoka <etokakingsley@gmail.com>
2022-11-21 16:14:51 +0100

    cleaned up the side menu

--- src/app/features/admin/core/component/sidebar/sidebar.component.ts
+++ src/app/features/admin/core/component/sidebar/sidebar.component.ts
@@ -4,68 +4,6 @@ import { Observable } from 'rxjs';
 import { adminActions, adminFeature } from '../../store';
 import { ParentNodeModel } from '../../model';
 
-const menuLinks = [
-  {
-    name: 'Dashboard',
-    icon: 'pi pi-th-large',
-    hasSubMenu: false,
-    link: '/dashboard',
-    isOpen: false,
-    subMenu :[]
-  },
-  {
-    name: 'Staff Management',
-    icon: 'pi pi-users',
-    hasSubMenu: true,
-    link: null,
-    isOpen: false,
-    subMenu :[
-      {
-        name: 'All Staff',
-        link: '/staffs'
-      },
-      {
-        name: 'Add New Staffs',
-        link: '/add-staff'
-      },
-    ]
-  },
-  {
-    name: 'Reports',
-    icon: 'pi pi-book',
-    hasSubMenu: true,
-    link: null,
-    isOpen: false,
-    subMenu :[
-      {
-        name: 'HSE Incidents',
-        link: '/hse-incidents'
-      },
-      {
-        name: 'CPA Incidents',
-        link: '/cpa-incidents'
-      },
-    ]
-  },
-  {
-    name: 'Project',
-    icon: 'pi pi-file-pdf',
-    hasSubMenu: true,
-    link: null,
-    isOpen: false,
-    subMenu :[
-      {
-        name: 'Add New Project',
-        link: '/project'
-      },
-      {
-        name: 'All Projects',
-        link: '/projects'
-      },
-    ]
-  }
-]
-
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -81,8 +19,7 @@ export class SidebarComponent implements OnInit {
   ngOnInit(): void {
   }
   
-  toggleMenu(index: number, ev: any){
-    //ev.preventDefault()
+  toggleMenu(index: number){
     this.store.dispatch(adminActions.toggleMenu({nodeIndex: index}))
   }
 
