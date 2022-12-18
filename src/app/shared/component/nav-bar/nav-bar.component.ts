398d2c8fd46320e34e988bc3817665ed4aac4717
Kingsley Etoka <etokakingsley@gmail.com>
2022-12-18 20:48:51 +0100

    fixed nsme display

--- src/app/shared/component/nav-bar/nav-bar.component.ts
+++ src/app/shared/component/nav-bar/nav-bar.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AuthenticatedData } from 'src/app/features/authentication/core/model';
+import { authFeature } from 'src/app/features/authentication/core/store';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,10 +10,13 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
+  user$: Observable<AuthenticatedData | null>
 
   @Input() pageTitle: string = '';
 
-  constructor() { }
+  constructor(private readonly store: Store) {
+    this.user$ = store.select(authFeature.selectUser)
+   }
 
   ngOnInit(): void {
   }
